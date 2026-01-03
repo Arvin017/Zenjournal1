1181636755c4752cbd553cb62c8d7bed976cf209
Arvin <arvin@example.com>
2026-01-03 22:38:25 +0530

    fix CORS for deployed frontend

--- backend/index.js
+++ backend/index.js
@@ -11,15 +11,29 @@ import { verifyJWT } from "./middlewares/auth.middleware.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, // Allow cookies to be sent with requests
-  })
-);
+import cors from "cors";
+
+app.use(cors());
+
+import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "https://zenjournal1.vercel.app"
+  ],
+  credentials: true
+}));
+
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true, // Allow cookies to be sent with requests
+//   })
+// );
 // app.use(cookieParser());
 
 connectDB();
